440a4cfe1241c9bf5042bde5ef21ef1d0d4a0b2d
Aliakwe <aliakwe@yahoo.com>
2019-09-18 19:22:49 +0100

    Updates

---
+++ precache-manifest.8bc711ec89a91924a14a14c4922b57fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5110e6d32e95a7fd1e3cf6932ac2f578",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "be995c1b14d5aa6d6ae0",
+    "url": "/contactmanager/static/css/main.0d779c84.chunk.css"
+  },
+  {
+    "revision": "dc70ead670743485a920",
+    "url": "/contactmanager/static/js/2.de521a48.chunk.js"
+  },
+  {
+    "revision": "be995c1b14d5aa6d6ae0",
+    "url": "/contactmanager/static/js/main.48becfe2.chunk.js"
+  },
+  {
+    "revision": "04a867495ba7868254b6",
+    "url": "/contactmanager/static/js/runtime~main.b5faa041.js"
+  }
+]);
